26714ee1244898cfe2ed8ea71694119eb03dd28a
nmskm240 <bokaronogod@gmail.com>
2023-01-09 21:50:57 +0900

    Icon image can be selected

---
+++ lib/component/form_builder__circle_avatar.dart
@@ -0,0 +1,91 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:image_cropper/image_cropper.dart';
+import 'package:image_picker/image_picker.dart';
+
+class FormBuilderCircleAvatar extends ConsumerWidget {
+  final String name;
+  final double radius;
+  final String defaultIconPath;
+  final String initialIcon;
+  late final StateProvider<String> circleAvatarProvider;
+  final void Function(String?)? onChanged;
+  final void Function(String?)? onSaved;
+  final void Function()? onReset;
+  final String Function(String?)? validator;
+
+  FormBuilderCircleAvatar({
+    super.key,
+    required this.name,
+    this.radius = 60,
+    this.defaultIconPath = "assets/images/default_avatar.png",
+    this.initialIcon = "",
+    this.onChanged,
+    this.onSaved,
+    this.onReset,
+    this.validator,
+  }) {
+    circleAvatarProvider = StateProvider<String>((ref) => initialIcon);
+  }
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return FormBuilderField<String>(
+      key: super.key,
+      name: name,
+      builder: (FormFieldState<String> field) {
+        return GestureDetector(
+          child: CircleAvatar(
+            backgroundImage: _loadIconOrDefault(ref),
+            radius: radius,
+          ),
+          onTap: () async {
+            await _onTap(ref);
+            field.didChange(ref.read(circleAvatarProvider));
+          },
+        );
+      },
+      onChanged: onChanged,
+      onSaved: onSaved,
+      onReset: onReset,
+      validator: validator,
+    );
+  }
+
+  ImageProvider _loadIconOrDefault(WidgetRef ref) {
+    return ref.watch(circleAvatarProvider).isEmpty
+        ? AssetImage(defaultIconPath) as ImageProvider
+        : MemoryImage(base64.decode(ref.read(circleAvatarProvider)));
+  }
+
+  Future<void> _onTap(WidgetRef ref) async {
+    final picked = await ImagePicker().pickImage(source: ImageSource.gallery);
+    if (picked == null) {
+      return;
+    }
+    final cropped = await ImageCropper().cropImage(
+      sourcePath: picked.path,
+      uiSettings: [
+        AndroidUiSettings(
+          toolbarTitle: "Icon",
+          initAspectRatio: CropAspectRatioPreset.square,
+          hideBottomControls: true,
+        ),
+        IOSUiSettings(
+          title: "Icon",
+          aspectRatioPickerButtonHidden: true,
+          hidesNavigationBar: true,
+        ),
+      ],
+      cropStyle: CropStyle.circle,
+    );
+    if (cropped == null) {
+      return;
+    }
+    final bytes = await cropped.readAsBytes();
+    ref.read(circleAvatarProvider.notifier).state = base64.encode(bytes);
+  }
+}
--- lib/view/page/friend_edit.dart
+++ lib/view/page/friend_edit.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_datetime_picker/flutter_datetime_picker.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:friend_list/component/listview_with_header.dart';
 import 'package:friend_list/provider/app_router_provider.dart';
+import 'package:friend_list/component/form_builder__circle_avatar.dart';
 
 class FriendEdit extends ConsumerWidget {
   const FriendEdit({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final formKey = GlobalKey<FormBuilderState>();
     return Scaffold(
       appBar: AppBar(
         actions: <Widget>[
@@ -21,168 +24,154 @@ class FriendEdit extends ConsumerWidget {
           ),
         ],
       ),
-      body: ListView(
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.all(5),
-            child: Form(
-              child: Column(
+      body: FormBuilder(
+        key: formKey,
+        child: ListView(
+          padding: const EdgeInsets.all(5),
+          children: <Widget>[
+            Center(
+              child: FormBuilderCircleAvatar(
+                name: "icon",
+                radius: 60,
+              ),
+            ),
+            Card(
+              child: ListViewWithHeader(
+                title: "Profile",
                 children: <Widget>[
-                  const CircleAvatar(
-                    radius: 60,
-                    child: Icon(
-                      Icons.person,
-                      size: 100,
-                    ),
-                  ),
-                  Card(
-                    child: ListViewWithHeader(
-                      title: "Profile",
-                      children: <Widget>[
-                        ListTile(
-                          title: TextFormField(
-                            decoration: const InputDecoration(
-                              label: Text("name"),
-                            ),
-                          ),
-                        ),
-                        ListTile(
-                          title: TextFormField(
-                            decoration: const InputDecoration(
-                              label: Text("nickname"),
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  Card(
-                    child: ListViewWithHeader(
-                      title: "Anniversary",
-                      trailing: IconButton(
-                        icon: const Icon(Icons.add),
-                        onPressed: () {
-                          // TODO: add process
-                        },
+                  ListTile(
+                    title: TextFormField(
+                      decoration: const InputDecoration(
+                        label: Text("name"),
                       ),
-                      children: <Widget>[
-                        ListTile(
-                          title: TextFormField(
-                            onTap: () {
-                              FocusScope.of(context).requestFocus(FocusNode());
-                              DatePicker.showDatePicker(
-                                context,
-                                maxTime: DateTime.now(),
-                              );
-                            },
-                            decoration: const InputDecoration(
-                              label: Text("Birthday"),
-                            ),
-                            controller: TextEditingController(
-                              text: "XXXX/YY/ZZ",
-                            ),
-                          ),
-                          trailing: IconButton(
-                            icon: const Icon(Icons.delete),
-                            onPressed: () {
-                              // TODO: delete process
-                            },
-                          ),
-                        ),
-                      ],
                     ),
                   ),
-                  Card(
-                    child: ListViewWithHeader(
-                      title: "Contact",
-                      trailing: IconButton(
-                        icon: const Icon(Icons.add),
-                        onPressed: () {
-                          // TODO: add process
-                        },
+                  ListTile(
+                    title: TextFormField(
+                      decoration: const InputDecoration(
+                        label: Text("nickname"),
                       ),
-                      children: <Widget>[
-                        ListTile(
-                          title: TextFormField(
-                            decoration: const InputDecoration(
-                              label: Text("Phone"),
-                            ),
-                            controller: TextEditingController(
-                              text: "000-0000-0000",
-                            ),
-                          ),
-                          trailing: IconButton(
-                            icon: const Icon(Icons.delete),
-                            onPressed: () {
-                              // TODO: delete process
-                            },
-                          ),
-                        ),
-                        ListTile(
-                          title: TextFormField(
-                            decoration: const InputDecoration(
-                              label: Text("Address"),
-                            ),
-                            controller: TextEditingController(
-                              text: "XXXXX XXXXXXX",
-                            ),
-                          ),
-                          trailing: IconButton(
-                            icon: const Icon(Icons.delete),
-                            onPressed: () {
-                              // TODO: delete process
-                            },
-                          ),
-                        ),
-                        ListTile(
-                          title: TextFormField(
-                            decoration: const InputDecoration(
-                              label: Text("Twitter"),
-                            ),
-                            controller: TextEditingController(
-                              text: "@XXXX",
-                            ),
-                          ),
-                          trailing: IconButton(
-                            icon: const Icon(Icons.delete),
-                            onPressed: () {
-                              // TODO: delete process
-                            },
-                          ),
-                        ),
-                      ],
                     ),
                   ),
-                  Card(
-                    child: ListViewWithHeader(
-                      title: "Tag",
-                      trailing: IconButton(
-                        icon: const Icon(Icons.add),
-                        onPressed: () {
-                          // TODO: add process
-                        },
+                ],
+              ),
+            ),
+            Card(
+              child: ListViewWithHeader(
+                title: "Anniversary",
+                trailing: IconButton(
+                  icon: const Icon(Icons.add),
+                  onPressed: () {
+                    // TODO: add process
+                  },
+                ),
+                children: <Widget>[
+                  ListTile(
+                    title: FormBuilderDateTimePicker(
+                      name: 'anniversaries.birthday',
+                      inputType: InputType.date,
+                      decoration: const InputDecoration(
+                        label: Text("Birthday"),
                       ),
+                    ),
+                    trailing: IconButton(
+                      icon: const Icon(Icons.delete),
+                      onPressed: () {
+                        // TODO: delete process
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Card(
+              child: ListViewWithHeader(
+                title: "Contact",
+                trailing: IconButton(
+                  icon: const Icon(Icons.add),
+                  onPressed: () {
+                    // TODO: add process
+                  },
+                ),
+                children: <Widget>[
+                  ListTile(
+                    title: TextFormField(
+                      decoration: const InputDecoration(
+                        label: Text("Phone"),
+                      ),
+                      controller: TextEditingController(
+                        text: "000-0000-0000",
+                      ),
+                    ),
+                    trailing: IconButton(
+                      icon: const Icon(Icons.delete),
+                      onPressed: () {
+                        // TODO: delete process
+                      },
+                    ),
+                  ),
+                  ListTile(
+                    title: TextFormField(
+                      decoration: const InputDecoration(
+                        label: Text("Address"),
+                      ),
+                      controller: TextEditingController(
+                        text: "XXXXX XXXXXXX",
+                      ),
+                    ),
+                    trailing: IconButton(
+                      icon: const Icon(Icons.delete),
+                      onPressed: () {
+                        // TODO: delete process
+                      },
+                    ),
+                  ),
+                  ListTile(
+                    title: TextFormField(
+                      decoration: const InputDecoration(
+                        label: Text("Twitter"),
+                      ),
+                      controller: TextEditingController(
+                        text: "@XXXX",
+                      ),
+                    ),
+                    trailing: IconButton(
+                      icon: const Icon(Icons.delete),
+                      onPressed: () {
+                        // TODO: delete process
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Card(
+              child: ListViewWithHeader(
+                title: "Tag",
+                trailing: IconButton(
+                  icon: const Icon(Icons.add),
+                  onPressed: () {
+                    // TODO: add process
+                  },
+                ),
+                children: <Widget>[
+                  ListTile(
+                    title: Wrap(
                       children: <Widget>[
-                        ListTile(
-                          title: Wrap(
-                            children: <Widget>[
-                              Chip(
-                                label: const Text("test"),
-                                onDeleted: () {
-                                  // TODO: delete process
-                                },
-                              )
-                            ],
-                          ),
-                        ),
+                        Chip(
+                          label: const Text("test"),
+                          onDeleted: () {
+                            // TODO: delete process
+                          },
+                        )
                       ],
                     ),
                   ),
                 ],
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
