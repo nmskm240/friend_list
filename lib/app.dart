5d810dcae3fcb9de868bc52d0ae3c9e3948267f3
nmskm240 <bokaronogod@gmail.com>
2023-01-09 22:45:40 +0900

    change theme

--- lib/app.dart
+++ lib/app.dart
@@ -12,20 +12,6 @@ class App extends ConsumerWidget {
       home: Router(
         routerDelegate: AppRouter(ref),
       ),
-      theme: ThemeData(
-        appBarTheme: const AppBarTheme(
-          backgroundColor: Color(0xFF495464),
-        ),
-        inputDecorationTheme: const InputDecorationTheme(
-          focusColor: Color(0xFF495464),
-        ),
-        scaffoldBackgroundColor: const Color(0xFFF4F4F2),
-        bottomNavigationBarTheme: const BottomNavigationBarThemeData(
-          selectedItemColor: Color(0xFF495464),
-        ),
-        bottomAppBarColor: const Color(0xFFE8E8E8),
-        cardColor: const Color(0xFFE8E8E8),
-      ),
     );
   }
 }
