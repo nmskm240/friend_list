056f71a93b9b24d68d60c18d781a3b69c8087368
nmskm240 <bokaronogod@gmail.com>
2023-02-01 21:37:34 +0900

    create anniversary repository interface

---
+++ lib/domain/anniversary/i_anniversary_repository.dart
@@ -0,0 +1,8 @@
+import 'package:friend_list/domain/anniversary/anniversary.dart';
+
+abstract class IAnniversaryRepository {
+  Future<Anniversary?> findByID(int id);
+  Future<Anniversary?> findByPersonID(int personId);
+  Future<Iterable<Anniversary>> getAll();
+  Future<Anniversary> save(Anniversary anniversary);
+}
