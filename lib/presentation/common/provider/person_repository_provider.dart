961f162c17f20f55696dd60a350c72b05cb67182
nmskm240 <UTC.AM240@gmail.com>
2023-02-22 22:57:20 +0900

    delete person repository provider

--- lib/presentation/common/provider/person_repository_provider.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:friend_list/infrastructure/person/person_repository.dart';
-import 'package:friend_list/presentation/common/provider/app_database_provider.dart';
-
-final personRepositoryProvider =
-    Provider((ref) => PersonRepository(ref.watch(appDatabaseProvider)));
--- lib/presentation/common/provider/person_service_provider.dart
+++ lib/presentation/common/provider/person_service_provider.dart
@@ -1,11 +1,12 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:friend_list/application/person_app_service.dart';
 import 'package:friend_list/infrastructure/person/person_factory.dart';
-import 'package:friend_list/presentation/common/provider/person_repository_provider.dart';
+import 'package:friend_list/infrastructure/person/person_repository.dart';
+import 'package:friend_list/presentation/common/provider/app_database_provider.dart';
 
 final personServiceProvider = Provider(
   (ref) => PersonAppService(
     PersonFactory(),
-    ref.watch(personRepositoryProvider),
+    PersonRepository(ref.watch((appDatabaseProvider))),
   ),
 );
