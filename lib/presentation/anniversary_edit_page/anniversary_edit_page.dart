c03b9f5dff9cd082e0c1ed8d553bb114e6cc7919
nmskm240 <UTC.AM240@gmail.com>
2023-02-26 11:17:27 +0900

    create drum roll date picker

--- lib/presentation/anniversary_edit_page/anniversary_edit_page.dart
+++ lib/presentation/anniversary_edit_page/anniversary_edit_page.dart
@@ -1,94 +1,45 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_datetime_picker/flutter_datetime_picker.dart';
-import 'package:friend_list/presentation/common/always_disabled_focus_node.dart';
-import 'package:friend_list/presentation/common/widget/list_view_with_header.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
+import 'package:form_builder_validators/form_builder_validators.dart';
+import 'package:friend_list/presentation/anniversary_edit_page/widget/form_builder_drum_roll_date_picker.dart';
 
 class AnniversaryEditPage extends StatelessWidget {
   const AnniversaryEditPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final key = GlobalKey<FormBuilderState>();
     return Scaffold(
       appBar: AppBar(
         actions: [
           IconButton(
             icon: const Icon(Icons.check),
             onPressed: () {
-              //TODO: save edit data
-              Navigator.of(context).pop();
+              if (key.currentState!.validate()) {
+                debugPrint(key.currentState!.instantValue.toString());
+                Navigator.of(context).pop();
+              }
             },
           ),
         ],
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(15),
-        child: Column(
+      body: FormBuilder(
+        key: key,
+        child: ListView(
+          padding: const EdgeInsets.all(15),
           children: [
-            const TextField(
-              decoration: InputDecoration(
+            FormBuilderTextField(
+              name: "name",
+              decoration: const InputDecoration(
                 label: Text("name"),
               ),
+              validator: FormBuilderValidators.required(),
             ),
-            TextField(
-              focusNode: AlwaysDisabledFocusNode(),
-              decoration: const InputDecoration(
-                label: Text("date"),
-              ),
-              onTap: () {
-                DatePicker.showDatePicker(context);
-                //TODO: set picked date to text field
-              },
+            FormBuilderDrumRollDatePicker(
+              name: "date",
+              validator: FormBuilderValidators.required(),
             ),
             const Divider(),
-            ListViewWithHeader(
-              scrollLock: true,
-              leading: const Text("Notification"),
-              action: IconButton(
-                icon: const Icon(Icons.add),
-                onPressed: () {
-                  //TODO: set new notification schedule
-                },
-              ),
-              children: <Widget>[
-                TextField(
-                  focusNode: AlwaysDisabledFocusNode(),
-                  controller: TextEditingController(text: "X day before"),
-                  decoration: InputDecoration(
-                    suffix: IconButton(
-                      icon: const Icon(Icons.delete),
-                      onPressed: () {
-                        //TODO: delete this notification
-                      },
-                    ),
-                  ),
-                  onTap: () {
-                    showModalBottomSheet(
-                      context: context,
-                      builder: (context) {
-                        final options = [
-                          "1 day before",
-                          "1 week before",
-                          "1 month before"
-                        ];
-                        return ListView.builder(
-                          itemCount: options.length,
-                          itemBuilder: (context, index) {
-                            return ListTile(
-                              title: Text(options[index]),
-                              onTap: () {
-                                //TODO: add to list
-                                Navigator.of(context).pop();
-                              },
-                            );
-                          },
-                        );
-                      },
-                    );
-                  },
-                ),
-                const Divider(),
-              ],
-            )
           ],
         ),
       ),
