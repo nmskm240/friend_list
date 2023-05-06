0ac1b66dfa8b5c60ab9afa5039ad95df4582e4ee
nmskm240 <UTC.AM240@gmail.com>
2023-05-06 10:52:15 +0900

    delete unused file

--- lib/application/model/contact_edit_settings.dart
+++ lib/application/model/contact_edit_settings.dart
@@ -1,11 +1,10 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
-import 'package:friend_list/application/navigation_service.dart';
 import 'package:friend_list/common/constant/contact_method.dart';
 
 part "contact_edit_settings.freezed.dart";
 
 @freezed
-class ContactEditSettings with _$ContactEditSettings implements RouteRequest {
+class ContactEditSettings with _$ContactEditSettings {
   const factory ContactEditSettings({
     @Default("") String id,
     required String name,
--- lib/application/provider/navigation_service_provider.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:friend_list/application/navigation_service.dart';
-
-final navigationServiceProvider = Provider<NavigationService>((ref) {
-  return NavigationService();
-});
