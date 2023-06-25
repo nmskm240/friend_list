056107dad33e1ef89e61472105d660a37b0da19e
nmskm240 <UTC.AM240@gmail.com>
2023-06-25 14:37:16 +0900

    fix layout

--- lib/presentation/person_detail_page/person_detail_page.dart
+++ lib/presentation/person_detail_page/person_detail_page.dart
@@ -9,139 +9,246 @@ import 'package:friend_list/presentation/person_detail_page/provider/person_deta
 import 'package:friend_list/presentation/person_detail_page/state/person_detail_page_state.dart';
 import 'package:intl/intl.dart';
 
-@RoutePage()
-class PersonDetailPage extends ConsumerWidget {
-  @protected
-  final AutoDisposeStateNotifierProvider<PersonDetailPageNotifier,
-      PersonDetailPageState> provider;
+final _localprovider = StateNotifierProvider.autoDispose<
+    PersonDetailPageNotifier, PersonDetailPageState>((ref) {
+  throw UnimplementedError();
+});
+final _tabs = {
+  "Anniversary": const _AnniversaryTabView(),
+  "Contact": const _ContactTabView(),
+};
 
-  PersonDetailPage({
+@RoutePage()
+class PersonDetailPage extends StatelessWidget {
+  final Person _person;
+
+  const PersonDetailPage({
     super.key,
     required Person person,
-  }) : provider = personDetailPage.call(person);
+  }) : _person = person;
+
+  @override
+  Widget build(BuildContext context) {
+    return ProviderScope(
+      overrides: [
+        _localprovider.overrideWith(
+            (ref) => ref.read(personDetailPage.call(_person).notifier)),
+      ],
+      child: const Scaffold(
+        appBar: _AppBar(),
+        body: _Body(),
+      ),
+    );
+  }
+}
+
+class _AppBar extends ConsumerWidget implements PreferredSizeWidget {
+  const _AppBar();
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final state = ref.watch(provider);
-    final notifier = ref.read(provider.notifier);
-    return Scaffold(
-      appBar: AppBar(
-        actions: <Widget>[
-          IconButton(
-            onPressed: () async {
-              await notifier.onPressedDeleteButton();
-              ref.read(router).pop();
-            },
-            icon: const Icon(Icons.delete),
-          )
-        ],
+    final notifier = ref.read(_localprovider.notifier);
+    return AppBar(
+      actions: <Widget>[
+        IconButton(
+          onPressed: () async {
+            await notifier.onPressedDeleteButton();
+            ref.read(router).pop();
+          },
+          icon: const Icon(Icons.delete),
+        )
+      ],
+    );
+  }
+}
+
+class _Body extends StatelessWidget {
+  const _Body();
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: _tabs.length,
+      child: NestedScrollView(
+        headerSliverBuilder: (context, innerBoxIsScrolled) {
+          return <Widget>[
+            const _Header(),
+            const _TabBar(),
+          ];
+        },
+        body: const _Main(),
       ),
-      body: CustomScrollView(
-        slivers: <Widget>[
-          SliverAppBar(
-            expandedHeight: 175,
-            leading: const Spacer(),
-            backgroundColor: Colors.transparent,
-            flexibleSpace: FlexibleSpaceBar(
-              background: Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.all(10),
-                  child: Row(
+    );
+  }
+}
+
+class _Header extends ConsumerWidget {
+  const _Header();
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final state = ref.watch(_localprovider);
+    return SliverList(
+      delegate: SliverChildListDelegate(
+        [
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(10),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: <Widget>[
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Center(
-                            child: CircleAvatar(
-                              radius: 45,
-                              backgroundImage: MemoryImage(state.icon),
-                            ),
-                          ),
-                          Text(
-                            state.name,
-                            style: Theme.of(context).textTheme.headlineSmall,
-                          ),
-                          Text(
-                            state.nickname,
-                            style: Theme.of(context).textTheme.titleSmall,
-                          ),
-                          Text(
-                            state.formatedAge,
-                            style: Theme.of(context).textTheme.caption,
-                          ),
-                        ],
+                      Center(
+                        child: CircleAvatar(
+                          radius: 45,
+                          backgroundImage: MemoryImage(state.icon),
+                        ),
                       ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: <Widget>[
-                          ElevatedButton(
-                            onPressed: () {
-                              showModalBottomSheet(
-                                context: context,
-                                shape: const RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.vertical(
-                                    top: Radius.circular(25.0),
-                                  ),
-                                ),
-                                builder: (builder) {
-                                  return ListView.builder(
-                                    itemCount: state.contacts.length,
-                                    itemBuilder: (context, index) {
-                                      final contact =
-                                          state.contacts.elementAt(index);
-                                      return ListTile(
-                                        leading: Icon(contact.method.icon),
-                                        title: Text(contact.method.name),
-                                        subtitle: Text(contact.value),
-                                      );
-                                    },
-                                  );
-                                },
-                              );
-                            },
-                            style: IconButton.styleFrom(
-                              shape: const CircleBorder(),
-                            ),
-                            child: const Icon(Icons.contacts),
-                          ),
-                          ElevatedButton.icon(
-                            onPressed: () async {
-                              await notifier.onPressedEditButton();
-                            },
-                            icon: const Icon(Icons.edit),
-                            label: const Text(Strings.editButtonLable),
-                            style: ElevatedButton.styleFrom(
-                              shape: const StadiumBorder(),
-                            ),
-                          ),
-                        ],
+                      Text(
+                        state.name,
+                        style: Theme.of(context).textTheme.headlineSmall,
+                      ),
+                      Text(
+                        state.nickname,
+                        style: Theme.of(context).textTheme.titleSmall,
+                      ),
+                      Text(
+                        state.formatedAge,
+                        style: Theme.of(context).textTheme.caption,
                       ),
                     ],
                   ),
-                ),
+                ],
               ),
             ),
           ),
-          SliverList(
-            delegate: SliverChildBuilderDelegate(
-              (context, index) {
-                final anniversary = state.anniversaries.elementAt(index);
-                return ListTile(
-                  title: Text(anniversary.name),
-                  subtitle: Text(DateFormat.yMd().format(anniversary.date)),
-                  trailing: const Icon(Icons.chevron_right),
-                  onTap: () async {
-                    await notifier.onPressedAnniveraryListTile(anniversary);
-                  },
-                );
-              },
-              childCount: state.anniversaries.length,
-            ),
-          )
         ],
       ),
+    );
+  }
+}
+
+class _TabBar extends StatelessWidget {
+  const _TabBar();
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverPersistentHeader(
+      floating: false,
+      pinned: true,
+      delegate: _TabBarDelegate(
+        bar: TabBar(
+          unselectedLabelColor: Colors.black45,
+          labelColor: Colors.black87,
+          tabs: _tabs.keys
+              .map(
+                (e) => Tab(
+                  child: Text(e),
+                ),
+              )
+              .toList(),
+        ),
+      ),
+    );
+  }
+}
+
+class _TabBarDelegate extends SliverPersistentHeaderDelegate {
+  const _TabBarDelegate({required this.bar});
+
+  final TabBar bar;
+
+  @override
+  double get maxExtent => bar.preferredSize.height;
+
+  @override
+  double get minExtent => bar.preferredSize.height;
+
+  @override
+  Widget build(
+      BuildContext context, double shrinkOffset, bool overlapsContent) {
+    return Container(
+      child: bar,
+    );
+  }
+
+  @override
+  bool shouldRebuild(_TabBarDelegate oldDelegate) {
+    return bar != oldDelegate.bar;
+  }
+}
+
+class _Main extends StatelessWidget {
+  const _Main();
+
+  @override
+  Widget build(BuildContext context) {
+    return TabBarView(
+      children: _tabs.values.toList(),
+    );
+  }
+}
+
+class _AnniversaryTabView extends ConsumerWidget {
+  const _AnniversaryTabView();
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final anniversaries =
+        ref.watch(_localprovider.select((value) => value.anniversaries));
+    final notifier = ref.read(_localprovider.notifier);
+    return CustomScrollView(
+      slivers: [
+        SliverList(
+          delegate: SliverChildBuilderDelegate(
+            (context, index) {
+              final anniversary = anniversaries.elementAt(index);
+              return ListTile(
+                title: Text(anniversary.name),
+                subtitle: Text(DateFormat.yMd().format(anniversary.date)),
+                trailing: const Icon(Icons.chevron_right),
+                onTap: () async {
+                  await notifier.onPressedAnniveraryListTile(anniversary);
+                },
+              );
+            },
+            childCount: anniversaries.length,
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class _ContactTabView extends ConsumerWidget {
+  const _ContactTabView();
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final contacts =
+        ref.watch(_localprovider.select((value) => value.contacts));
+    return CustomScrollView(
+      slivers: [
+        SliverList(
+          delegate: SliverChildBuilderDelegate(
+            (context, index) {
+              final contact = contacts.elementAt(index);
+              return ListTile(
+                title: Text(contact.name),
+                subtitle: Text(contact.value),
+                leading: Icon(contact.method.icon),
+              );
+            },
+            childCount: contacts.length,
+          ),
+        ),
+      ],
     );
   }
 }
